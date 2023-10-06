2c92832025a22874d82eeaa66562cb06183f2222
carlosq-mv <cquiroz7869@gmail.com>
2023-10-05 22:19:17 -0700

    First Commit

---
+++ merge.py
@@ -0,0 +1,19 @@
+def merge_list(list1, list2):
+    list3 = list1 + list2
+
+    print(list3)
+
+    pivot = len(list3) - 1
+    first = 0
+    last = pivot
+
+    while first < pivot:
+
+
+    print(pivot)
+
+
+list1 = [1,5,3,7]
+list2 = [6,2,4]
+
+merge_list(list1, list2)
